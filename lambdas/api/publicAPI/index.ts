8cf2de644f7495809b9737475d9d1d2df4ff064c
Harry Breen <harrytjbreen@gmail.com>
2025-10-07 21:34:58 +0100

    fix: add logging in API handler and map custom domain to API Gateway default stage

--- lambdas/api/publicAPI/index.ts
+++ lambdas/api/publicAPI/index.ts
@@ -3,5 +3,7 @@ import {router} from "./src/routes";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const { path, httpMethod } = event;
+  console.log(`Path: ${path}`);
+  console.log(`Method: ${httpMethod}`);
     return await router(path, httpMethod, event);
 };
