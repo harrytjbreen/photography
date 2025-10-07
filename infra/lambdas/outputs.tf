631248076f8f2d844e15ae41e8043a1cdac04402
Harry Breen <harrytjbreen@gmail.com>
2025-10-07 18:51:39 +0100

    fmt

--- infra/lambdas/outputs.tf
+++ infra/lambdas/outputs.tf
@@ -1,14 +1,14 @@
 output "lambda_arns" {
   description = "Map of Lambda function ARNs by name"
-  value = { for k, v in aws_lambda_function.lambda : k => v.arn }
+  value       = { for k, v in aws_lambda_function.lambda : k => v.arn }
 }
 
 output "lambda_invoke_arns" {
   description = "Map of Lambda invoke ARNs by name"
-  value = { for k, v in aws_lambda_function.lambda : k => v.invoke_arn }
+  value       = { for k, v in aws_lambda_function.lambda : k => v.invoke_arn }
 }
 
 output "lambda_names" {
   description = "Map of Lambda function names by name"
-  value = { for k, v in aws_lambda_function.lambda : k => v.function_name }
+  value       = { for k, v in aws_lambda_function.lambda : k => v.function_name }
 }
