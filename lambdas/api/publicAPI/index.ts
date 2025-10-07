d4caee65e4efc2fcaf9bdb4872caef55c4a12f97
Harry Breen <harrytjbreen@gmail.com>
2025-10-07 21:37:22 +0100

    logging

--- lambdas/api/publicAPI/index.ts
+++ lambdas/api/publicAPI/index.ts
@@ -3,7 +3,6 @@ import {router} from "./src/routes";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const { path, httpMethod } = event;
-  console.log(`Path: ${path}`);
-  console.log(`Method: ${httpMethod}`);
+  console.log(`Received request: ${event}`);
     return await router(path, httpMethod, event);
 };
